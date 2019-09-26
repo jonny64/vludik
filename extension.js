2cd3959888059d69a4e158037c47334f89d4f855
Maksim Gruzdev <mgruzdev@cheby.ru>
2019-09-26 11:36:47 +0300

    focusFunction

--- extension.js
+++ extension.js
@@ -5,12 +5,25 @@ function activate(context) {
 
     console.log('starting vludik');
 
+    function focusFunction( prefix ) {
+        let editor = vscode.window.activeTextEditor;
+        var uri = vscode.Uri.file(editor.document.fileName);
+        vscode.commands.executeCommand('vscode.executeDocumentSymbolProvider', uri).then(syms => {
+            let sym = syms.find(function(i) {return i.name.startsWith(prefix)})
+            editor.revealRange(sym.location.range)
+        });
+    }
+
     context.subscriptions.push(vscode.commands.registerCommand('extension.goto_select', function () {
-        open_view('Content');
+        open_view('Content').then(() => {
+            focusFunction ('select_')
+        });
     }));
 
     context.subscriptions.push(vscode.commands.registerCommand('extension.goto_draw', function () {
-        open_view('Presentation');
+        open_view('Presentation').then(() => {
+            focusFunction ('draw_')
+        });
     }));
 
     context.subscriptions.push(vscode.commands.registerCommand('extension.goto_model', function () {
@@ -21,15 +34,15 @@ function activate(context) {
         var view_file = path.parse(vscode.window.activeTextEditor.document.fileName);
         var lib_dir = path.dirname(view_file.dir);
         var file_path = path.join(lib_dir, view, view_file.name + '.' + 'pm');
-        vscode.workspace.openTextDocument(file_path).then(doc => {
+        return vscode.workspace.openTextDocument(file_path).then(doc => {
             vscode.window.showTextDocument(doc);
             console.log('opened ' + file_path);
-        });
+        })
     }
 }
+
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
 function deactivate() {
 }
 exports.deactivate = deactivate;
