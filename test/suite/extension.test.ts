15c82e20a07249f6ef29d13ed94c8ecde1db39a0
Maksim Gruzdev <mgruzdev@cheby.ru>
2021-09-07 22:02:23 +0300

    copy type test FIX

--- test/suite/extension.test.ts
+++ test/suite/extension.test.ts
@@ -6,32 +6,27 @@ import * as fs from 'fs'
 import * as vscode from 'vscode'
 const no_slices_workspace = '../../test/workspaces/elu_dia_slick_template'
 
-suite('Extension Test Suite', () => {
+suite ('Vludik Test Suite', () => {
 
-	console.log ('Start all tests.')
-
-	let extension : vscode.Extension<any>|undefined
+	const as_is = path.join(__dirname, no_slices_workspace, 'back/lib/Content/users.js')
+	const to_be = path.join(__dirname, no_slices_workspace, 'back/lib/Content/users_copy.js')
 
 	before (async () => {
-		extension = await vscode.extensions.getExtension('jonny64.vludik')
 		console.log (`open ${no_slices_workspace}...`)
 		let uri = vscode.Uri.file (no_slices_workspace)
 		await vscode.commands.executeCommand('vscode.openFolder', uri)
-	})
-
-	it ('should load and activate extension', async () => {
-		assert.ok (extension)
-		assert.ok (extension.isActive)
+		if (fs.existsSync (to_be)) {
+			console.log (`rm ${to_be}`)
+			await fs.unlinkSync (to_be)
+		}
 	})
 
 	it ('should copy type in project without slices', async () => {
-		const as_is = path.join(__dirname, no_slices_workspace, 'back/lib/Content/users.js')
-		const to_be = path.join(__dirname, no_slices_workspace, 'back/lib/Content/users_copy.js')
-
 		const uri = vscode.Uri.file (as_is)
-		await vscode.workspace.openTextDocument(uri)
+		let doc = await vscode.workspace.openTextDocument(uri)
+		await vscode.window.showTextDocument(doc)
 		await vscode.commands.executeCommand ('extension.copy_type', {new_type: 'users_copy'})
 
 		assert (fs.existsSync (to_be), to_be)
-	});
-});
+	})
+})
